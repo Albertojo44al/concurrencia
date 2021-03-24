616cc14e38682618455810abb1b6ad63d0b0922e
Albertojo44al <Albertojo44al@unitec.edu>
2021-03-23 21:00:42 -0600

    examen

---
+++ Examen-2/DistributedLibrary-master/DistributedLibrary.Gateway/Services/BookService.cs
@@ -0,0 +1,20 @@
+﻿using DistributedLibrary.Gateway.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DistributedLibrary.Gateway.Services
+{
+    public class BookService
+    {
+        private readonly HttpClient _httpClient;
+        public async Task<IEnumerable<BookDto>> GetBookById(Guid isdn)
+        {
+            var books = 0;
+            return JsonConvert.DeserializeObject<IEnumerable<BookDto>>(books);
+        }
+    }
+}
